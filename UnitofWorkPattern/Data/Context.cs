4b07c71394845482ee0fee30c5ee64b23716b846
Manjiri G <manjiri.g@symbioenergy.co.uk>
2020-10-26 14:36:26 +0000

    Changes

---
+++ UnitofWorkPattern/Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitofWorkPattern.Data
+{
+    public class Context :DbContext
+    {
+        public Context(DbContextOptions<Context> option): base(options)
+        {
+
+        }
+        protected  override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ UnitofWorkPattern/Infrastructure/IStudentRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitofWorkPattern.Models;
+
+namespace UnitofWorkPattern.Infrastructure
+{
+   public interface IStudentRepo
+    {
+        IList<Student> GetAll();
+        Student GetById(int Id);
+        void Insert(Student student);
+        void Update(Student student);
+        void Delete(Student student);
+
+    }
+}
